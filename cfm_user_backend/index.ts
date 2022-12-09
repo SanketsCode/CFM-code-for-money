84407cc98b3f5d206c2406ffef3118f5cd2b4f64
sanket <programmersanket@gmail.com>
2022-12-09 22:24:55 +0530

    Working started

---
+++ cfm_user_backend/index.ts
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+
+require('dotenv/config');
+
+
+mongoose.set('strictQuery',false);
+mongoose.connect(process.env.DATABASE,{
+    useNewUrlParser : true,
+    useUnifiedTopology: true,
+    dbName:'CFM_DATA',
+}).then(() => {
+    console.log("Database Connected");
+}).catch((err : string) => {
+    console.log(err);
+    console.log("Database not connected");
+})
+
+//Server Build Kiya
+app.listen(process.env.PORT,() => {
+    console.log("Listening to port ",process.env.PORT);
+})
+
---
+++ cfm_user_frontend/pages/works/[id].tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function SpecificWorkPage() {
+  return (
+    <div>SpecificWorkPage</div>
+  )
+}
